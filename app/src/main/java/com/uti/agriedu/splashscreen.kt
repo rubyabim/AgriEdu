2bcf873d4c12d4eb989d00538406ff08bcd38201
Abim Febriansyah <abimfebriansyah2000@gmail.com>
2025-05-21 09:57:04 +0700

    Membuat Splashscreen Aplikasi

---
+++ app/src/main/java/com/uti/agriedu/splashscreen.kt
@@ -0,0 +1,24 @@
+package com.uti.agriedu
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import com.uti.agriedu.R
+
+class splashscreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_splashscreen)
+
+        // Splash delay 2 detik
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 2000)
+    }
+}
