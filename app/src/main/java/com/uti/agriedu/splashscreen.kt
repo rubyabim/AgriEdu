30d2a8595b3962dd05890156266b847a61757d5c
Abim Febriansyah <abimfebriansyah2000@gmail.com>
2025-05-21 19:07:58 +0700

    mengganti wana Button navigator bar

--- app/src/main/java/com/uti/agriedu/splashscreen.kt
+++ app/src/main/java/com/uti/agriedu/splashscreen.kt
@@ -4,20 +4,16 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import com.uti.agriedu.R
 
 class splashscreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_splashscreen)
+        setContentView(R.layout.activity_splashscreen) // layout splashscreen.xml kamu
 
-        // Splash delay 2 detik
+        // Tunggu 2 detik lalu pindah ke MainActivity
         Handler(Looper.getMainLooper()).postDelayed({
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
+            startActivity(Intent(this, MainActivity::class.java))
             finish()
         }, 2000)
     }
