7ab03b28d6e529e948575bf4cac8729bbcfafdc9
dzilan <radityadzilan@gmail.com>
2025-06-04 00:21:41 +0700

    connect button tanaman to TanamanFragment

--- app/src/main/java/com/uti/agriedu/Home.kt
+++ app/src/main/java/com/uti/agriedu/Home.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.uti.agriedu.databinding.FragmentHomeBinding
+import com.uti.agriedu.databinding.FragmentTanamanBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -33,8 +35,15 @@ class Home : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        val binding = FragmentHomeBinding.inflate(layoutInflater)
+
+        binding.buttontanaman.setOnClickListener {
+            (requireActivity() as MainActivity).supportFragmentManager.beginTransaction().replace(R.id.frame_layout, TanamanFragment()).commit()
+        }
+
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        return binding.root
     }
 
     companion object {
