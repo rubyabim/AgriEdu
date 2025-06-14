8580eb007826f70611d19c29249390abe3b3cfb8
dzilan <radityadzilan@gmail.com>
2025-06-14 22:49:10 +0700

    setting dan about

--- app/src/main/java/com/uti/agriedu/InformationFragment.kt
+++ app/src/main/java/com/uti/agriedu/InformationFragment.kt
@@ -5,8 +5,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.uti.agriedu.databinding.FragmentSettingBinding
-
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -15,10 +13,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [Setting.newInstance] factory method to
+ * Use the [InformationFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class Setting : Fragment() {
+class InformationFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -35,12 +33,8 @@ class Setting : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val binding = FragmentSettingBinding.inflate(layoutInflater)
-        binding.ButtonInformasi.setOnClickListener {
-            (requireActivity() as MainActivity).supportFragmentManager.beginTransaction().replace(R.id.frame_layout, InformationFragment()).commit()
-        }
         // Inflate the layout for this fragment
-        return binding.root
+        return inflater.inflate(R.layout.fragment_information, container, false)
     }
 
     companion object {
@@ -50,12 +44,12 @@ class Setting : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment Setting.
+         * @return A new instance of fragment InformationFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            Setting().apply {
+            InformationFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
