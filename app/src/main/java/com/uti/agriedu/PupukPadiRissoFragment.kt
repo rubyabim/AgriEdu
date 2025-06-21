e1afe9ba3a9a784089f730ec0626f45d72004078
dzilan <radityadzilan@gmail.com>
2025-06-21 20:50:51 +0700

    menambahkan Fungsi Kembali Dari fragment padi risso ke fragment pupuk

--- app/src/main/java/com/uti/agriedu/PupukPadiRissoFragment.kt
+++ app/src/main/java/com/uti/agriedu/PupukPadiRissoFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.uti.agriedu.databinding.FragmentPupukPadirissoBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -32,9 +33,11 @@ class PupukPadiRissoFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_pupuk_padirisso, container, false)
+    ): View? {val binding = FragmentPupukPadirissoBinding.inflate(layoutInflater)
+        binding.tombolKembali.setOnClickListener {
+            (requireActivity() as MainActivity).supportFragmentManager.beginTransaction().replace(R.id.frame_layout, PupukFragment()).commit()
+        }
+        return binding.root
     }
 
     companion object {
