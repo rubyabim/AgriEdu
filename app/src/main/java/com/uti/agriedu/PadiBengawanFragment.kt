fdf42b9819938fad599f75d522977771f79e55d6
Abim Febriansyah <abimfebriansyah2000@gmail.com>
2025-06-22 14:03:48 +0700

    Mengganti binding = FragmentPadiBengawanBinding

--- app/src/main/java/com/uti/agriedu/PadiBengawanFragment.kt
+++ app/src/main/java/com/uti/agriedu/PadiBengawanFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.uti.agriedu.databinding.FragmentKetanBinding
+import com.uti.agriedu.databinding.FragmentPadiBengawanBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -32,9 +34,12 @@ class PadiBengawanFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_padi_bengawan, container, false)
+    ): View? {val binding = FragmentPadiBengawanBinding.inflate(layoutInflater)
+        binding.tombolKembali.setOnClickListener {
+            (requireActivity() as MainActivity).supportFragmentManager.beginTransaction().replace(R.id.frame_layout,
+                TanamanFragment()).commit()
+        }
+        return binding.root
     }
 
     companion object {
