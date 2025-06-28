9be3ec41d79ac6c0b82ad6bbe6910dec835b4c9b
hamsaif <ilhamilhamilham05@gmail.com>
2025-06-28 20:50:05 +0700

    memperbaiki bug binding benih unggul *tadi lupa ubah alamat binding

--- app/src/main/java/com/uti/agriedu/BenihUnggul.kt
+++ app/src/main/java/com/uti/agriedu/BenihUnggul.kt
@@ -5,7 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.uti.agriedu.databinding.FragmentBenihPrimaBinding
+import com.uti.agriedu.databinding.FragmentBenihUnggulBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -34,7 +34,7 @@ class BenihUnggul : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val binding = FragmentBenihPrimaBinding.inflate(layoutInflater)
+        val binding = FragmentBenihUnggulBinding.inflate(layoutInflater)
         binding.tombolKembali.setOnClickListener {
             (requireActivity() as MainActivity).supportFragmentManager.beginTransaction().replace(R.id.frame_layout, BenihFragment()).commit()
         }
