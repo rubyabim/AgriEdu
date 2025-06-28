0d743cc9b9b556ab42796c4e71197b41f9d4bf43
hamsaif <ilhamilhamilham05@gmail.com>
2025-06-28 20:21:18 +0700

    menambahkan binding tombol kembali benih unggul

--- app/src/main/java/com/uti/agriedu/BenihUnggul.kt
+++ app/src/main/java/com/uti/agriedu/BenihUnggul.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.uti.agriedu.databinding.FragmentBenihPrimaBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -33,8 +34,11 @@ class BenihUnggul : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_benih_unggul, container, false)
+        val binding = FragmentBenihPrimaBinding.inflate(layoutInflater)
+        binding.tombolKembali.setOnClickListener {
+            (requireActivity() as MainActivity).supportFragmentManager.beginTransaction().replace(R.id.frame_layout, BenihFragment()).commit()
+        }
+        return binding.root
     }
 
     companion object {
