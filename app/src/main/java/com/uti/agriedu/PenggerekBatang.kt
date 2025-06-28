a4547b92798321f4b0bf7b66f0d5ff4413fe7b8f
hamsaif <ilhamilhamilham05@gmail.com>
2025-06-28 21:04:07 +0700

    membuat binding untuk penggerak batang

--- app/src/main/java/com/uti/agriedu/PenggerekBatang.kt
+++ app/src/main/java/com/uti/agriedu/PenggerekBatang.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.uti.agriedu.databinding.FragmentPenggerekBatangBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -33,8 +34,9 @@ class PenggerekBatang : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        val binding = FragmentPenggerekBatangBinding.inflate(layoutInflater)
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_penggerek_batang, container, false)
+        return binding.root
     }
 
     companion object {
